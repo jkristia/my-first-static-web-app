bc0ca4bf39781a50cab1a381021da8413094d0db
Jesper Kristiansen <jesper.kristiansen@keysight.com>
2022-01-20 15:44:57 -0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>{{message}}</div>`,
+  template: `<div> - {{message}} - </div>`,
 })
 export class AppComponent {
   message = 'howdy';
 
   constructor(private http: HttpClient) {
+    console.log('Lets begin')
+    this.load()
+
     // this.http.get('/api/message')
-    //   .subscribe((resp: any) => this.message = resp.text);
+    //   .subscribe((resp: any) => {
+    //     console.log('resp ', resp)
+    //     this.message = resp.text
+    //   });
+  }
+  async load() {
+    console.log('>> load')
+    const resp: any = await this.http.get('/api/message').toPromise()
+    console.log('<< load', resp)
+    this.message = resp.message || 'oh oh'
   }
 }
