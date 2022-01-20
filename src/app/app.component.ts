4df50f0f575c8ba21c7a7bb2e90a49e4effb9761
Jesper Kristiansen <jesper.kristiansen@keysight.com>
2022-01-20 15:11:20 -0800

    function test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}} Tooooo</div>`,
+  template: `<div>{{message}}</div>`,
 })
 export class AppComponent {
-  value = 'World';
+  message = 'howdy';
+
+  constructor(private http: HttpClient) {
+    // this.http.get('/api/message')
+    //   .subscribe((resp: any) => this.message = resp.text);
+  }
 }
